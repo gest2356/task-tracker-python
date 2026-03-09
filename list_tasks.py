39245676f8faf1b8f8454e32c49113db2baa4f14
gest2356 <vojta.javurek@gmail.com>
2026-03-09 16:37:15 +0100

    changed: list now print id

--- list_tasks.py
+++ list_tasks.py
@@ -2,7 +2,7 @@ import json
 import os
 import time
 from datetime import datetime
-from time import strftime
+from xml.etree.ElementTree import tostring
 
 
 def list_tasks():
@@ -17,11 +17,10 @@ def list_tasks():
             dt_obj_crt = datetime.fromisoformat(created_at)
             created_at_formated = dt_obj_crt.strftime('%Y-%m-%d %H:%M:%S')
 
-
             updated_at = task['updated_at']
             dt_obj_update = datetime.fromisoformat(updated_at)
             updated_at_formated = dt_obj_update.strftime('%Y-%m-%d %H:%M:%S')
 
-            print(task['description'] + " " + task['status'] +  " " +  created_at_formated + " " + updated_at_formated)
+            print(f"{task['id']} {task['description']} {task['status']} {created_at_formated} {updated_at_formated}")
     else:
         print("No tasks found")
